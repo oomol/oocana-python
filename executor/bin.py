23492e4f207e48ae4c7486b6a93b5ffec18e476b
yleaf <11785335+leavesster@users.noreply.github.com>
2024-03-01 14:42:03 +0800

    add long live executor and subscribe

--- executor/bin.py
+++ executor/bin.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python
 
 import sys
-import importlib
-import importlib.util
+import json
 import os
 import traceback
+import importlib
+import importlib.util
+from vocana import setup_vocana_sdk, Mainframe
 
-from vocana import setup_vocana_sdk
-
-def run():
-    sdk = setup_vocana_sdk()
-    try:
-        index_module = load_module(get_source())
-        index_module.main(sdk.props, sdk)
-    except Exception:
-        traceback_str = traceback.format_exc()
-        sdk.send_error(traceback_str)
-        sys.exit(1)
-
-def get_source():
-    source = sys.argv[1]
-    if source is None or source == "":
-        raise Exception("source file path is empty")
-    return source.strip()
+mainframe
 
 def load_module(source):
     if (os.path.isabs(source)):
@@ -37,4 +23,26 @@ def load_module(source):
     file_spec.loader.exec_module(module)
     return module
 
-run()
+def setup():
+    # 考虑启动方式，以及获取地址以及执行器名称，or default value
+    address = os.environ.get('VOCANA_ADDRESS') if os.environ.get('VOCANA_ADDRESS') else 'mqtt://127.0.0.1:8080'
+    executor = os.environ.get('VOCANA_EXECUTOR') if os.environ.get('VOCANA_EXECUTOR') else 'python-executor'
+    global mainframe
+    mainframe = Mainframe(address)
+    mainframe.connect()
+
+    def run(message):
+        try:
+            start_args = json.loads(message.payload)
+            sdk = setup_vocana_sdk(mainframe, start_args['session_id'], start_args['job_id'])
+            index_module = load_module(start_args['source'])
+            index_module.main(sdk.props, sdk)
+        except Exception:
+            traceback_str = traceback.format_exc()
+            mainframe.send_error(traceback_str)
+            sys.exit(1)
+
+    mainframe.subscribe(f'{executor}', run)
+
+
+setup()

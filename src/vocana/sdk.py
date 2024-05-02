c52952a06116a5856ffd078118e93dabd566f21a
leavesster <11785335+leavesster@users.noreply.github.com>
2024-05-02 14:08:06 +0800

    feat: add success log and output error (#30)
    * fix: add error log when output key is not in def
    
    * chore remove store obj log
    
    * chore: add out of define key example
    
    * fix: add success log

--- src/vocana/sdk.py
+++ src/vocana/sdk.py
@@ -63,9 +63,12 @@ class VocanaSDK:
             output_def = self.__outputs.get(handle)
             if output_def is not None and output_def.get('data') and output_def.get('data').get('type') == 'var':
                 ref = self.__store_ref(handle)
-                print(f'store output {handle} as object ref')
                 self.__store[ref] = output
                 v = asdict(ref)
+        
+        if self.__outputs is not None and self.__outputs.get(handle) is None:
+            # TODO: 未来添加 warning 级别日志时，更改为 warning 而不是 error
+            self.send_error(f'Output handle key: [{handle}] is not defined in Block outputs schema.')
 
         node_result = {
             'type': 'BlockOutput',

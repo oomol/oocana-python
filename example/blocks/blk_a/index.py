2add512f05123763b420df72f3414ef92953952a
yleaf <11785335+leavesster@users.noreply.github.com>
2024-05-09 11:24:51 +0800

    chore: update sdk version

--- example/blocks/blk_a/index.py
+++ example/blocks/blk_a/index.py
@@ -6,6 +6,6 @@ def main(props, context):
     context.log_json({"message": "Hello from Python blk_a"})
     obj = datetime.datetime.now()
     print("obj", obj)
-    context.output("122", "my_output")
+    context.output(obj, "my_output")
     context.output("321321", "my_output1")
     context.done()

92ce0caa3048f92daa9bbb3fd3acc9f7e4876ddc
leavesster <11785335+leavesster@users.noreply.github.com>
2024-05-15 16:01:30 +0800

    fix:  fix wrong logging format and migration to new API version (#45)
    * fix: wrong logging format
    
    * feat: update to new callback api
    
    * chore: follow paho mqtt migration

--- src/vocana/mainframe.py
+++ src/vocana/mainframe.py
@@ -1,5 +1,6 @@
 import json
 import paho.mqtt.client as mqtt
+from paho.mqtt.enums import CallbackAPIVersion
 import operator
 from urllib.parse import urlparse
 import uuid
@@ -28,7 +29,8 @@ class Mainframe:
         url = urlparse(connect_address)
 
         self.client = mqtt.Client(
-            client_id=f"python-executor-{uuid.uuid4().hex[:8]}", clean_session=False
+            callback_api_version=CallbackAPIVersion.VERSION2,
+            client_id=f"python-executor-{uuid.uuid4().hex[:8]}"
         )
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
@@ -37,22 +39,28 @@ class Mainframe:
         self.client.loop_start()
         return self.client
 
-    # https://stackoverflow.com/a/57396505/4770006 在 on_connect 回调里面订阅的 topic，在重连时，会自动重新订阅，其他地方调用 subscribe 需要自己处理重新订阅逻辑。
-    def on_connect(self, client: mqtt.Client, userdata, flags, rc):
-        logger.info("connect to broker success")
+    # mqtt v5 重连后，订阅和队列信息会丢失(v3 在初始化时，设置 clean_session 后，会保留两者。
+    # 我们的 broker 使用的是 v5，在 on_connect 里订阅，可以保证每次重连都重新订阅上。
+    def on_connect(self, client, userdata, flags, reason_code, properties):
+        if reason_code != 0:
+            logger.error("connect to broker failed, reason_code: %s", reason_code)
+            return
+        else:
+            logger.info("connect to broker success")
+
         client.subscribe(f"executor/{name}/execute", qos=1)
         client.subscribe(f"executor/{name}/drop", qos=1)
 
     def on_connect_fail(self) -> None:
         logger.error("connect to broker failed")
 
-    def on_disconnect(self, client, userdata, rc):
-        logger.warning("disconnect to broker, rc: %s", rc)
+    def on_disconnect(self, client, userdata, flags, reason_code, properties):
+        logger.warning("disconnect to broker, reason_code: %s", reason_code)
         self.on_ready = False
 
     def send(self, job_info: JobDict, msg):
         if self.on_ready is False:
-            logger.error("SDK is not ready when send message", job_info, msg)
+            logger.error("SDK is not ready when send message {} {}".format(job_info, msg))
             raise Exception("SDK is not ready when send message")
 
         info = self.client.publish(
@@ -62,7 +70,7 @@ class Mainframe:
 
     def report(self, block_info: BlockDict, msg: dict):
         if self.on_ready is False:
-            logger.error("SDK is not ready when report message", block_info, msg)
+            logger.error("SDK is not ready when report message {} {}".format(block_info, msg))
             raise Exception("SDK is not ready when report message")
         info = self.client.publish("report", json.dumps({**block_info, **msg}), qos=1)
         info.wait_for_publish()
@@ -87,14 +95,14 @@ class Mainframe:
 
         while True:
             if replay is not None:
-                logger.info("notify ready success", session_id, job_id)
+                logger.info("notify ready success in {} {}".format(session_id, job_id))
                 return replay
 
     def subscribe_drop(self, callback):
         topic = f"executor/{name}/drop"
 
         def on_message(_client, _userdata, message):
-            logger.info("drop message", message.payload)
+            logger.info("drop message: {}".format(message.payload))
             payload = json.loads(message.payload)
             callback(payload)
 
@@ -104,7 +112,7 @@ class Mainframe:
         topic = f"executor/{name}/execute"
 
         def on_message(_client, _userdata, message):
-            logger.info("execute message", message.payload)
+            logger.info("execute message: {}".format(message.payload))
             payload = json.loads(message.payload)
             callback(payload)
 

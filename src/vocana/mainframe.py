44b12153266a55fad633a14175f03d790256b4c0
leavesster <11785335+leavesster@users.noreply.github.com>
2024-05-15 17:56:37 +0800

    refactor: remove ready check, don't wait message publish and add test case (#46)
    * refactor: use mqtt state to check not bool
    
    * chore: add test case
    
    * fix: remove ready check because it has false negative case
    
    * feat: do not wait for publish just return
    
    * refactor: fix notify_ready parameters
    
    * update
    
    * chore: version

--- src/vocana/mainframe.py
+++ src/vocana/mainframe.py
@@ -14,11 +14,9 @@ logger = logging.getLogger(__name__)
 class Mainframe:
     address: str
     client: mqtt.Client
-    on_ready: bool
 
     def __init__(self, address: str) -> None:
         self.address = address
-        self.on_ready = False
 
     def connect(self):
         connect_address = (
@@ -27,7 +25,11 @@ class Mainframe:
             else f"mqtt://{self.address}"
         )
         url = urlparse(connect_address)
-
+        client = self._setup_client()
+        client.connect(host=url.hostname, port=url.port) # type: ignore
+        client.loop_start()
+    
+    def _setup_client(self):
         self.client = mqtt.Client(
             callback_api_version=CallbackAPIVersion.VERSION2,
             client_id=f"python-executor-{uuid.uuid4().hex[:8]}"
@@ -35,8 +37,6 @@ class Mainframe:
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
         self.client.on_connect_fail = self.on_connect_fail # type: ignore
-        self.client.connect(host=url.hostname, port=url.port) # type: ignore
-        self.client.loop_start()
         return self.client
 
     # mqtt v5 重连后，订阅和队列信息会丢失(v3 在初始化时，设置 clean_session 后，会保留两者。
@@ -56,42 +56,34 @@ class Mainframe:
 
     def on_disconnect(self, client, userdata, flags, reason_code, properties):
         logger.warning("disconnect to broker, reason_code: %s", reason_code)
-        self.on_ready = False
 
-    def send(self, job_info: JobDict, msg):
-        if self.on_ready is False:
-            logger.error("SDK is not ready when send message {} {}".format(job_info, msg))
-            raise Exception("SDK is not ready when send message")
-
-        info = self.client.publish(
+    # 不等待 publish 完成，使用 qos 参数来会保证消息到达。
+    def send(self, job_info: JobDict, msg) -> mqtt.MQTTMessageInfo:
+        return self.client.publish(
             f'session/{job_info["session_id"]}', json.dumps({"job_id": job_info["job_id"], "session_id": job_info["session_id"], **msg}), qos=1
         )
-        info.wait_for_publish()
 
-    def report(self, block_info: BlockDict, msg: dict):
-        if self.on_ready is False:
-            logger.error("SDK is not ready when report message {} {}".format(block_info, msg))
-            raise Exception("SDK is not ready when report message")
-        info = self.client.publish("report", json.dumps({**block_info, **msg}), qos=1)
-        info.wait_for_publish()
+    def report(self, block_info: BlockDict, msg: dict) -> mqtt.MQTTMessageInfo:
+        return self.client.publish("report", json.dumps({**block_info, **msg}), qos=1)
 
-    def notify_ready(self, msg):
+    def notify_ready(self, session_id: str, job_id: str) -> dict:
 
-        session_id = msg.get("session_id")
-        job_id = msg.get("job_id")
         topic = f"inputs/{session_id}/{job_id}"
         replay = None
 
         def on_message_once(_client, _userdata, message):
             nonlocal replay
-            self.on_ready = True
             self.client.unsubscribe(topic)
             replay = json.loads(message.payload)
 
         self.client.subscribe(topic, qos=1)
         self.client.message_callback_add(topic, on_message_once)
 
-        self.client.publish(f"session/{session_id}", json.dumps(msg), qos=1)
+        self.client.publish(f"session/{session_id}", json.dumps({
+            "type": "BlockReady",
+            "session_id": session_id,
+            "job_id": job_id,
+        }), qos=1)
 
         while True:
             if replay is not None:

5d9fa18f09e1876b6f903f9bdc894a06fee39529
yleaf <11785335+leavesster@users.noreply.github.com>
2024-05-18 11:54:12 +0800

    chore: rename dir name

--- scripts/publish_vocana.sh
+++ scripts/publish_vocana.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./dist
 pdm build
-mkdir -p dist/vocana-sdk-python
-mkdir -p dist/vocana-sdk-python/dist
+mkdir -p dist/oocana-sdk-python
+mkdir -p dist/oocana-sdk-python/dist
 node scripts/make_npm_package.js
-cd dist/vocana-sdk-python && npm publish
+cd dist/oocana-sdk-python && npm publish

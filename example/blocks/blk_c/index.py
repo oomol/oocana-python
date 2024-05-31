e89348b3a89bad9fc1f10b1547ddc08efdc986b2
yleaf <11785335+leavesster@users.noreply.github.com>
2024-05-31 16:11:57 +0800

    chore: add type hint for example

--- example/blocks/blk_c/index.py
+++ example/blocks/blk_c/index.py
@@ -1,6 +1,7 @@
 import datetime
+from oocana import Context
 
-def main(props, context):
+def main(props, context: Context):
 
     context.send_message("Hello from Python blk_a")
     context.log_json({"message": "Hello from Python blk_a"})

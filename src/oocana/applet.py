f31d2ac7827219ca409a48b7754f6a4a7147b740
yleaf <11785335+leavesster@users.noreply.github.com>
2024-06-23 16:21:39 +0800

    feat: add applet types

---
+++ src/oocana/applet.py
@@ -0,0 +1,32 @@
+from typing import Callable, Any, TypedDict, Optional
+from .context import Context
+from enum import Enum
+from .data import JobDict, HandleDict
+
+class AppletStopOption(Enum):
+    BLOCK = "block_end"
+    SESSION = "session_end"
+    App = "app_end"
+    Never = "never"
+
+class AppletStartOption(Enum):
+    BLOCK = "block_start"
+    SESSION = "session_start"
+    App = "app_start"
+
+class AppletContext(TypedDict):
+    blockHandler: dict[str, Callable[[Any, Context], Any]] | Callable[[str, Any, Context], Any]
+
+class AppletExecutor(TypedDict):
+    name: str
+    entry: str
+    function: str
+    start_at: Optional[AppletStartOption]
+    stop_at: Optional[AppletStopOption]
+    keep_alive: Optional[int]
+
+class AppletExecutePayload(JobDict):
+    dir: str
+    block_name: str
+    applet_executor: AppletExecutor
+    outputs: dict[str, HandleDict]

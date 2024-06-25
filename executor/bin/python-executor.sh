9d3fadf78f1649f144fba0d9de54d8587cfe376d
leavesster <11785335+leavesster@users.noreply.github.com>
2024-06-25 18:46:03 +0800

    fix: wrong relative path (#66)

--- executor/bin/python-executor.sh
+++ executor/bin/python-executor.sh
@@ -2,9 +2,7 @@
 set -x
 
 DIR=$(cd $(dirname "$0"); pwd -P)
-# 这个脚本会被 nodejs 包管理工具放置在 node_modules/.bin 目录下。
-# 为了降低对系统的依赖，手动根据 node_modules 文件结构 cd 回 Package 的目录，而不是使用 readlink 或者 stat 等系统调用。
-# 因为没有 set -e，所以 cd 失败不会导致脚本退出，而是继续执行 python 命令。
-cd $DIR/../@oomol/python-executor
+# 虽然复制到 bin 里面了，但是执行的时候，是resolve 到当前项目内再执行的。因此相对路径是不变的。
+cd $DIR/..
 
 python -m impl.executor "$@"

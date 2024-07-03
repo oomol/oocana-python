f4ec4bc5f86d280e1c110cdd5be046e6e34cbb9d
leavesster <11785335+leavesster@users.noreply.github.com>
2024-07-03 13:32:27 +0800

    chore(ci): remove package version use script to sync (#78)

--- scripts/publish_executor.sh
+++ scripts/publish_executor.sh
@@ -1,5 +1,11 @@
 #/bin/sh -e
 
+DIR=$(cd $(dirname "$0"); pwd)
+cd $DIR/..
+
+./scripts/sync_version.js executor
+mkdir -p dist/python-executor
 cp -r executor/* dist/python-executor
-cd dist/python-executor && npm publish
+cd dist/python-executor
+npm publish
 rm -rf ./dist/python-executor
--- scripts/publish_oocana.sh
+++ scripts/publish_oocana.sh
@@ -1,7 +1,11 @@
 #/bin/sh -e
 
+DIR=$(cd $(dirname "$0"); pwd)
+cd $DIR/..
+
+./scripts/sync_version.js oocana
 (cd oocana && pdm build)
 mkdir -p oocana/dist/npm_package/dist
 node scripts/make_oocana_npm_package.js
 cd oocana/dist/npm_package
-# npm publish
+npm publish

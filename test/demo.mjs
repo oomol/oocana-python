f0a885350b8fd7170c27be3a194b3c0e1f2b06f6
yleaf <11785335+leavesster@users.noreply.github.com>
2024-07-26 17:47:40 +0800

    refactor(test): fix test example and add broker

---
+++ test/demo.mjs
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-env node */
+
+import  remitter from "remitter";
+import { Vocana } from "@oomol/oocana";
+import path from "node:path"
+import { exit, exitCode } from "node:process";
+import { fileURLToPath } from "node:url";
+
+const cli = new Vocana();
+
+cli.events.on(remitter.ANY_EVENT, m => console.log(m));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+console.log("__dirname", __dirname);
+
+async function main() {
+  await cli.connect();
+  await cli.runFlow({
+    flowPath: path.join(__dirname),
+    blockSearchPaths: [
+      path.join(__dirname, "blocks"),
+    ].join(","),
+  });
+
+  cli.events.on("BlockFinished", (event) => {
+    if (event["error"]) {
+      console.error("BlockFinished with error", event)
+      exitCode(-1)
+    }
+  })
+
+  cli.events.on("SessionFinished", () => {
+    console.log("SessionFinished")
+    exit(0)
+  })
+}
+
+main();

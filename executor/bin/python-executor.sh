730fed252c92dce9b7fb1a0962a417c28f7f58d1
yleaf <11785335+leavesster@users.noreply.github.com>
2024-08-22 16:59:21 +0800

    refactor: remove unused bin file

--- executor/bin/python-executor.sh
+++ executor/bin/python-executor.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # set -x
 
-# pnpm 的 bin 是 pnpm 额外创建一个 shell，有那个文件通过 shell 调用当前文件。
-# npm 的 bin 则是软链接到当前文件。
-# 先使用 readlink 获取真实文件路径，然后获取文件所在目录。这样可以兼容 npm 和 pnpm。readlink 在大多数系统中都存在。
+# pnpm 对 bin 的处理：
+#   pnpm 在 .bin 额外创建一个 shell，由那个 shell 调用本文件
+# npm 对 bin 的处理：
+#   在 bin 里直接创建一个到本文件的软链接
+
+# 先尝试使用 readlink 获取真实文件路径，来兼容 npm 和 pnpm。
+# readlink 在大多数系统中都存在。
 if command -v readlink >/dev/null 2>&1; then
   DIR=$(dirname "$(readlink -f "$0")")
 else

8b44e0145ed4b373ffde3926a5f16072bf34b0de
yleaf <11785335+leavesster@users.noreply.github.com>
2024-08-29 15:35:07 +0800

    test: add services block example

---
+++ test/packages/app/services/one/main.py
@@ -0,0 +1,15 @@
+def block_a_handler(inputs: dict, context):
+    print("Hello from block_a_handler")
+    return {"two": "22"}
+
+def block_b_handler(inputs: dict, context):
+    print("Hello from block_b_handler")
+    return {"two": "2222", "one": "11111"}
+
+def main(service):
+    print("Hello from service")
+    service["block_handler"] = {
+        "a": block_a_handler,
+        "b": block_b_handler
+    }
+    

339aaaf2acde647fdfec5563ced3257910194aeb
yleaf <11785335+leavesster@users.noreply.github.com>
2024-08-30 16:56:35 +0800

    chore: add is instance test

---
+++ flow-examples/flows/isinstance/both/__init__.py
@@ -0,0 +1,23 @@
+from oocana import Context
+
+class Both:
+    def __init__(self):
+        pass
+
+    def a(self):
+        return 
+
+    def b(self):
+        return
+
+def a():
+    return {"a":Both()}
+
+def b(inputs: dict, context: Context):
+    print("b in put is ", inputs)
+    i = inputs["b"]
+    
+    if isinstance(i, Both):
+        print("b in put isinstance is ", isinstance(i, Both))
+    else:
+        exit(1) # 这样来达到 assert 的效果

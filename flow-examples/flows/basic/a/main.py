9ab3de11e6710a552749a1c1c1816ce7b52a6d97
yleaf <11785335+leavesster@users.noreply.github.com>
2024-09-06 15:26:17 +0800

    test: add basic test for some use case

---
+++ flow-examples/flows/basic/a/main.py
@@ -0,0 +1,12 @@
+from oocana import Context
+import time
+
+def main(inputs, context: Context):
+
+    a = object()
+
+    for i in range(11):
+        context.report_progress(i * 10)
+        time.sleep(0.1)
+
+    return {"a": a}

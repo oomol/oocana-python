d6820a93e3285818d1fcc6286452366a7135817b
yleaf <11785335+leavesster@users.noreply.github.com>
2024-12-09 18:21:37 +0800

    feat: replace var

--- executor/python_executor/context.py
+++ executor/python_executor/context.py
@@ -1,5 +1,5 @@
 import logging
-from oocana import Mainframe, Context, StoreKey, BlockInfo
+from oocana import Mainframe, Context, StoreKey, BlockInfo, InputHandleDef
 from typing import Any, Dict
 from .secret import replace_secret
 
@@ -9,6 +9,7 @@ logger = logging.getLogger("EXECUTOR_NAME")
 def createContext(
     mainframe: Mainframe, session_id: str, job_id: str, store, output, session_dir: str
 ) -> Context:
+    
 
     node_props = mainframe.notify_block_ready(session_id, job_id)
 
@@ -18,10 +19,24 @@ def createContext(
     if inputs_def is not None and inputs is not None:
         inputs = replace_secret(inputs, inputs_def, node_props.get("inputs_def_patch"))
 
+        for k, v in inputs.items():
+            if inputs_def.get(k) is None:
+                continue
+            input_def = InputHandleDef(**inputs_def.get(k, {}))
+            if input_def.is_var_handle():
+                try:
+                    ref = StoreKey(**v)
+                except:  # noqa: E722
+                    logger.warning(f"not valid object ref: {v}")
+                    continue
+                if ref in store:
+                    inputs[k] = store.get(ref)
+                else:
+                    logger.error(f"object {ref} not found in store")
+
+
     if inputs is None:
         inputs = {}
-
-    
     
     blockInfo = BlockInfo(**node_props)
 

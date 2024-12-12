40eaef9be1540d4c433d64165d1c759c8faf40d1
yleaf <11785335+leavesster@users.noreply.github.com>
2024-12-12 12:08:56 +0800

    chore: add bin flow test

---
+++ flow-examples/flows/bin/scriptlets/+python#1.py
@@ -0,0 +1,11 @@
+from oocana import Context
+from base64 import b64encode, b64decode
+def main(inputs: dict, context: Context):
+
+  # your code
+
+  # generate a binary file
+
+  binary_data = b'\x01\x02\x03\x04'
+
+  return { "out": binary_data }

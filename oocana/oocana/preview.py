ee259a39574dae5a3ed6ba5175e4340cd9dab0e1
CRIMX <straybugs@gmail.com>
2024-12-16 12:56:05 +0800

    feat(type): add more preview payload types (#236)

--- oocana/oocana/preview.py
+++ oocana/oocana/preview.py
@@ -1,5 +1,5 @@
 
-from typing import Any, TypedDict, List, Literal, TypeAlias
+from typing import Any, TypedDict, List, Literal, TypeAlias, Union
 
 class TablePreviewData(TypedDict):
     columns: List[str | int | float]
@@ -10,8 +10,31 @@ class TablePreviewPayload(TypedDict):
     type: Literal['table']
     data: TablePreviewData | Any
 
+class TextPreviewPayload(TypedDict):
+    type: Literal["text"]
+    data: Any
+
+class JSONPreviewPayload(TypedDict):
+    type: Literal["json"]
+    data: Any
+
+class ImagePreviewPayload(TypedDict):
+    type: Literal['image']
+    data: str | List[str]
+
 class MediaPreviewPayload(TypedDict):
-    type: Literal['image', 'audio', 'video', 'markdown', "iframe"]
+    type: Literal["image", 'video', 'audio', 'markdown', "iframe", "html"]
     data: str
 
-PreviewPayload: TypeAlias = TablePreviewPayload | MediaPreviewPayload | Any
+class DefaultPreviewPayload:
+    type: str
+    data: Any
+
+PreviewPayload: TypeAlias = Union[
+    TablePreviewPayload,
+    TextPreviewPayload,
+    JSONPreviewPayload,
+    ImagePreviewPayload,
+    MediaPreviewPayload,
+    DefaultPreviewPayload
+]

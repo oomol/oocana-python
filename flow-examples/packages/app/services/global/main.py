f61c156d158a927b966955686e25aa0e5d6ed9dc
yleaf <11785335+leavesster@users.noreply.github.com>
2024-12-19 18:23:19 +0800

    chore: split service to global service

--- flow-examples/packages/app/services/global/main.py
+++ flow-examples/packages/app/services/global/main.py
@@ -1,11 +1,11 @@
 from oocana import ServiceContextAbstractClass
 
-def block_a_handler(inputs: dict, context):
-    print("Hello from block_a_handler")
-    return {"two": "22"}
+def block_b_handler(inputs: dict, context):
+    print("Hello from block_b_handler")
+    return {"two": "2222", "one": "11111"}
 
 def main(service: ServiceContextAbstractClass):
     print("Hello from service")
     service.block_handler = {
-        "a": block_a_handler,
+        "b": block_b_handler
     }

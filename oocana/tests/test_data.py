ff60064fa98efd2856f951a835464d9f4a11dc28
yleaf <11785335+leavesster@users.noreply.github.com>
2025-01-09 11:39:36 +0800

    feat: support encode dataclass directly

--- oocana/tests/test_data.py
+++ oocana/tests/test_data.py
@@ -50,3 +50,22 @@ class TestData(unittest.TestCase):
         self.assertEqual(block_info.job_id, "job_id_one")
         self.assertEqual(block_info.stacks, ["stack1", "stack2"])
         self.assertEqual(block_info.block_path, "block_path_one")
+
+    def test_dataclass_dumps(self):
+        block_info_dict = {
+            "session_id": "session_id_one",
+            "job_id": "job_id_one",
+            "stacks": ["stack1", "stack2"],
+            "block_path": "block_path_one",
+            "extra": "extra"
+        }
+
+        block_info = data.BlockInfo(**block_info_dict)
+        serialize_block_info = data.dumps(block_info)
+        self.assertEqual(serialize_block_info, '{"session_id": "session_id_one", "job_id": "job_id_one", "stacks": ["stack1", "stack2"], "block_path": "block_path_one"}')
+
+        list_serialize_block_info = data.dumps([block_info])
+        self.assertEqual(list_serialize_block_info, '[{"session_id": "session_id_one", "job_id": "job_id_one", "stacks": ["stack1", "stack2"], "block_path": "block_path_one"}]')
+
+        key_serialize_block_info = data.dumps({"key": block_info})
+        self.assertEqual(key_serialize_block_info, '{"key": {"session_id": "session_id_one", "job_id": "job_id_one", "stacks": ["stack1", "stack2"], "block_path": "block_path_one"}}')

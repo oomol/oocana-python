36d213a6c95095bd92b3c29b526ebda3b5e85e4e
leavesster <11785335+leavesster@users.noreply.github.com>
2025-03-27 15:32:50 +0800

    fix: use origin system api when called outside of block

--- executor/python_executor/data.py
+++ executor/python_executor/data.py
@@ -1,5 +1,5 @@
 from contextvars import ContextVar
 from oocana import Context, EXECUTOR_NAME  # noqa: F401
 
-block_var: ContextVar[Context] = ContextVar('context')
+block_var: ContextVar[Context] = ContextVar('block-context')
 store = {}

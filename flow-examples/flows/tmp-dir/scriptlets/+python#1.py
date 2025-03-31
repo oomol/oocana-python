93b80f9eb33046a33baa78d8e110447ac2b63a33
leavesster <11785335+leavesster@users.noreply.github.com>
2025-03-31 11:56:29 +0800

    chore: add test case

--- flow-examples/flows/tmp-dir/scriptlets/+python#1.py
+++ flow-examples/flows/tmp-dir/scriptlets/+python#1.py
@@ -1,3 +1,5 @@
+from oocana import Context
+import os.path
 #region generated meta
 import typing
 class Inputs(typing.TypedDict):
@@ -6,10 +8,16 @@ class Outputs(typing.TypedDict):
     output: str
 #endregion
 
-from oocana import Context
-
 def main(params: Inputs, context: Context) -> Outputs:
 
     assert(context.tmp_dir is not None)
+    assert(context.tmp_pkg_dir is not None)
+
+    # check if tmp_dir is exist
+    if not os.path.exists(context.tmp_dir):
+        raise Exception(f"tmp_dir {context.tmp_dir} is not exist")
+    # check if tmp_pkg_dir is exist
+    if not os.path.exists(context.tmp_pkg_dir):
+        raise Exception(f"tmp_pkg_dir {context.tmp_pkg_dir} is not exist") 
 
     return { "output": "output_value" }

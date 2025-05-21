74bd6d8bb74ce7fefb87cad64cd6aa19a9139080
hyrious <hyrious@outlook.com>
2025-05-21 11:46:11 +0800

    feat: add extra llm types to oocana (#350)

--- oocana/oocana/__init__.py
+++ oocana/oocana/__init__.py
@@ -3,5 +3,6 @@ from .context import *  # noqa: F403
 from .service import *  # noqa: F403
 from .handle_data import *  # noqa: F403
 from .preview import *  # noqa: F403
+from .extra import *  # noqa: F403
 from .schema import *  # noqa: F403
 from .mainframe import Mainframe as Mainframe  # noqa: F403

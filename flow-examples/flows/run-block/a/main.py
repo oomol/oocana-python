a1293c8839f66c081e2b0e08bd8d91d58a90a7fd
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-09 14:44:12 +0800

    feat: add run block error example

--- flow-examples/flows/run-block/a/main.py
+++ flow-examples/flows/run-block/a/main.py
@@ -6,6 +6,10 @@ async def main(inputs, context: Context):
     run_res.add_event_callback(lambda payload: print("event callback", payload))
     run_res.add_output_callback(lambda handle, value: print("output callback", handle, value))
     res = await run_res.finish()
-    print("block finished with response:", res)
+    assert res.get("error") is None
+
+    run_res = context.run_block("blk_bccc", {"my_input": "111"})
+    res = await run_res.finish()
+    assert res.get("error") is not None
 
     return {"a": "a"}

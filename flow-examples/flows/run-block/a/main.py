3a9b42574de72d6b656e5bc30700894aa326eac1
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-14 14:46:37 +0800

    test: use new parameters

--- flow-examples/flows/run-block/a/main.py
+++ flow-examples/flows/run-block/a/main.py
@@ -2,13 +2,13 @@ from oocana import Context
 
 async def main(inputs, context: Context):
 
-    run_res = context.run_block("blk_b", {"my_input": "111"})
+    run_res = context.run_block("blk_b", inputs={"my_input": "111"})
     run_res.add_event_callback(lambda payload: print("event callback", payload))
     run_res.add_output_callback(lambda handle, value: print("output callback", handle, value))
     res = await run_res.finish()
     assert res.get("error") is None
 
-    run_res = context.run_block("blk_bccc", {"my_input": "111"})
+    run_res = context.run_block("blk_bccc", inputs={"my_input": "111"})
     res = await run_res.finish()
     assert res.get("error") is not None
 

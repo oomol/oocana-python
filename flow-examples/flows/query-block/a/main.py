f8bb38767aca6a063c8a9191b6831d47f086d52c
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-18 10:38:21 +0800

    test: add test assert

--- flow-examples/flows/query-block/a/main.py
+++ flow-examples/flows/query-block/a/main.py
@@ -5,6 +5,12 @@ async def main(inputs, context: Context):
     run_res = await context.query_block("blk_b")
     assert run_res is not None
 
+    assert isinstance(run_res, dict), "Expected run result to be a dictionary"
+    assert "inputs_def" in run_res, "Expected 'inputs_def' key to be present in the run result"
+    assert "outputs_def" in run_res, "Expected 'outputs_def' key to be present in the run result"
+    assert "additional_outputs" in run_res, "Expected 'additional_outputs' key to be present in the run result"
+    assert "additional_inputs" in run_res, "Expected 'additional_inputs' key to be present in the run result"
+
     fail = False
     try:
         run_res = await context.query_block("blk_b_non_existent")

867a999a1dd37ae7a3c19311ffa79c1c0f880e53
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-21 14:14:16 +0800

    refactor: clear callback when finish

--- oocana/oocana/context.py
+++ oocana/oocana/context.py
@@ -789,6 +789,7 @@ class Context:
         def set_future_and_clean(error: None | str = None):
             self.__mainframe.remove_report_callback(event_callback)
             self.__mainframe.remove_request_response_callback(self.session_id, request_id, response_callback)
+            outputs_callbacks.clear()
 
             def set_future():
                 if future.done():

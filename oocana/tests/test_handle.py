a725ebb198355071b4843eba78bd758c13d36e05
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-24 14:14:22 +0800

    test: improve handle test

--- oocana/tests/test_handle.py
+++ oocana/tests/test_handle.py
@@ -1,66 +1,148 @@
 import unittest
-from oocana.handle import HandleDef, InputHandleDef, FieldSchema
+from oocana import HandleDef, InputHandleDef, FieldSchema, OutputHandleDef
 from typing import cast
 
+missing_handle = {
+}
 
-fixture = {
+redundant_handle = {
     "handle": "test",
     "json_schema": {
         "contentMediaType": "oomol/bin"
     },
+    "no_used_field": "test"
+}
+
+simple_handle = {
+    "handle": "test",
+}
+
+value_input_handle = {
+    "handle": "test",
+    "value": "test_value",
+}
+
+bin_handle = {
+    "handle": "test",
+    "json_schema": {
+        "contentMediaType": "oomol/bin"
+    },
+}
+
+var_handle = {
+    "handle": "test",
+    "json_schema": {
+        "contentMediaType": "oomol/var"
+    },
     "name": "options"
 }
 
+secret_handle = {
+    "handle": "test",
+    "json_schema": {
+        "contentMediaType": "oomol/secret"
+    }
+}
+
+serializable_var_input_handle = {
+    "handle": "test",
+    "json_schema": {
+        "contentMediaType": "oomol/var"
+    },
+    "serialize_for_cache": True
+}
+
+serializable_var_output_handle = {
+    "handle": "test",
+    "json_schema": {
+        "contentMediaType": "oomol/var"
+    },
+    "__serialize_for_cache": True
+}
+
 class TestHandleData(unittest.TestCase):
-    def test_handle_def_field(self):
 
-        d = fixture.copy()
-        HandleDef(**d)
-
-        d = fixture.copy()
-        del d["name"]
-
-        handle_def = HandleDef(**d)
-        self.assertEqual(handle_def.handle, "test")
-        self.assertIsNotNone(handle_def.json_schema)
-        
-        json_schema = cast(FieldSchema, handle_def.json_schema)
-        self.assertEqual(json_schema.contentMediaType, "oomol/bin")
-
-        d = fixture.copy()
-        del d["name"]
-        del d["json_schema"]
-
-        handle_def = HandleDef(**d)
-        self.assertEqual(handle_def.handle, "test")
-        self.assertIsNone(handle_def.json_schema)
-
-    def test_handle_def_extra_field(self):
-        d = fixture.copy()
-        d["a"] = "a"
-
-        handle_def = HandleDef(**d)
-        self.assertEqual(handle_def.handle, "test")
-
-    def test_handle_def_missing_field(self):
-        d = {
-            "a": "1",
-        }
-
+    def test_error_handle(self):
+        """Test that error handle raises ValueError."""
         with self.assertRaises(ValueError, msg="missing attr key: 'handle'"):
-            InputHandleDef(**d) # type: ignore
+            HandleDef(**missing_handle)
 
-    def test_handle_type(self):
-        d = fixture.copy()
-        d["json_schema"]["contentMediaType"] = "oomol/secret"
+    def test_simple_handle(self):
+        """Test that simple handle can be created."""
+        handle_def = HandleDef(**simple_handle)
+        self.assertEqual(handle_def.handle, "test")
 
-        handle_def = HandleDef(**d)
+
+        # get attributes
+        self.assertEqual(handle_def.get("handle"), "test")
+        self.assertEqual(handle_def["handle"], "test")
+        self.assertTrue("handle" in handle_def)
+
+        self.assertIsNone(handle_def.description)
+        self.assertIsNone(handle_def.json_schema)
+        self.assertIsNone(handle_def.kind)
+        self.assertIsNone(handle_def.nullable)
+        self.assertIsNone(handle_def.is_additional)
+
+        self.assertFalse(handle_def.is_var_handle())
+        self.assertFalse(handle_def.is_secret_handle())
+        self.assertFalse(handle_def.is_bin_handle())
+
+        # input
+        input_def = InputHandleDef(**simple_handle)
+        self.assertEqual(input_def.handle, "test")
+        self.assertFalse(input_def.is_var_handle())
+        self.assertFalse(input_def.is_secret_handle())
+        self.assertFalse(input_def.is_bin_handle())
+        
+        self.assertIsNone(input_def.value)
+        self.assertFalse(input_def.has_value())
+
+        # output
+        output_def = OutputHandleDef(**simple_handle)
+        self.assertEqual(output_def.handle, "test")
+        self.assertFalse(output_def.is_var_handle())
+        self.assertFalse(output_def.is_secret_handle())
+        self.assertFalse(output_def.is_bin_handle())
+
+        self.assertFalse(output_def.need_serialize_var_for_cache())
+
+    def test_redundant_bin_handle(self):
+        """Test that redundant fields in handle are ignored."""
+        handle_def = HandleDef(**redundant_handle)
+        self.assertEqual(handle_def.handle, "test")
+
+        self.assertEqual(handle_def.get("no_used_field"), "test")
+
+    def test_value_input_handle(self):
+        """Test that input handle with value can be created."""
+        input_def = InputHandleDef(**value_input_handle)
+        self.assertEqual(input_def.handle, "test")
+        self.assertEqual(input_def.value, "test_value")
+
+        self.assertTrue(input_def.has_value())
+
+    def test_serializable_var_input_handle(self):
+        """Test that serializable var input handle can be created."""
+        input_def = InputHandleDef(**serializable_var_input_handle)
+        self.assertEqual(input_def.handle, "test")
+        self.assertTrue(input_def.is_var_handle())
+        self.assertTrue(input_def.serialize_for_cache)
+
+    def test_serializable_var_output_handle(self):
+        """Test that serializable var output handle can be created."""
+        output_def = OutputHandleDef(**serializable_var_output_handle)
+        self.assertEqual(output_def.handle, "test")
+        self.assertTrue(output_def.is_var_handle())
+        self.assertTrue(output_def.need_serialize_var_for_cache())
+
+    def test_secret_handle(self):
+
+        handle_def = HandleDef(**secret_handle)
         self.assertTrue(handle_def.is_secret_handle())
 
-        d["json_schema"]["contentMediaType"] = "oomol/var"
-        handle_def = HandleDef(**d)
-        self.assertTrue(handle_def.is_var_handle())
 
+    def test_object_handle(self):
         d = {
             "handle": "auto_slices",
             "json_schema": {
@@ -76,34 +158,6 @@ class TestHandleData(unittest.TestCase):
             }
         }
         handle_def = HandleDef(**d)
-        self.assertFalse(handle_def.is_var_handle())
-        self.assertFalse(handle_def.is_secret_handle())
-
-    def test_input_handle_type(self):
-        d = fixture.copy()
-        d["json_schema"]["contentMediaType"] = "oomol/secret"
-
-        handle_def = InputHandleDef(**d)
-        self.assertTrue(handle_def.is_secret_handle())
-
-        d["json_schema"]["contentMediaType"] = "oomol/var"
-        handle_def = InputHandleDef(**d)
-        self.assertTrue(handle_def.is_var_handle())
-
-        d = {
-            "handle": "auto_slices",
-            "json_schema": {
-                "items": {
-                    "properties": {
-                        "begin": { "type": "number" },
-                        "end": { "type": "number" }
-                    },
-                    "required": ["begin", "end"],
-                    "type": "object"
-                    },
-                "type": "array"
-            }
-        }
-        handle_def = InputHandleDef(**d)
+        self.assertFalse(handle_def.is_bin_handle())
         self.assertFalse(handle_def.is_var_handle())
         self.assertFalse(handle_def.is_secret_handle())

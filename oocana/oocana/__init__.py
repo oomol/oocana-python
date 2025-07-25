139f682cfee0260e0f61ef2d593cc206cddac642
leavesster <11785335+leavesster@users.noreply.github.com>
2025-07-25 11:37:06 +0800

    feat: expose setup_dataframe_serialization and CompressionOptions (#410)

--- oocana/oocana/__init__.py
+++ oocana/oocana/__init__.py
@@ -6,3 +6,4 @@ from .preview import *  # noqa: F403
 from .extra import *  # noqa: F403
 from .schema import *  # noqa: F403
 from .mainframe import Mainframe as Mainframe  # noqa: F403
+from .serialization import setup_dataframe_serialization, CompressionOptions  # noqa: F403

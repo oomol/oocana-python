9ebe7cb7a629012bcf4a51e6f20452a413f7d91e
leavesster <11785335+leavesster@users.noreply.github.com>
2025-09-26 09:52:59 +0800

    refactor: use three componet

--- executor/python_executor/credential.py
+++ executor/python_executor/credential.py
@@ -8,17 +8,22 @@ class CredentialInput:
 def generate_credential_input(credential_path: str) -> CredentialInput | None:
     """Generate a CredentialInput from a credential path string.
 
-    The credential path should be in the format `${{OO_CREDENTIAL:type,id}}`. If the format is incorrect,
+    The credential path should be in the format `${{OO_CREDENTIAL:type,name,id}}`. If the format is incorrect,
     the function returns None.
     """
 
     if not (credential_path.startswith("${{OO_CREDENTIAL:") and credential_path.endswith("}}")):
+        # logger warning("Credential path format is incorrect. Expected to start with '${{OO_CREDENTIAL:' and end with '}}'.")
+        return None
+    
+    if credential_path.count(",") != 3:
+        # logger warning("Credential path format is incorrect. Expected exactly three commas.")
         return None
 
     credential_path = credential_path.removeprefix("${{OO_CREDENTIAL:").removesuffix("}}")
     if credential_path:
         try:
-            type, id = credential_path.split(",", maxsplit=1)
+            type, name, id = credential_path.split(",", maxsplit=2)
             return CredentialInput(type, id)
         except ValueError:
             return None
